40af3166b37e0c1add672891df91cc26217fa8db
Kair <kair8m@gmail.com>
2022-12-21 14:57:01 +0100

    add input param to cdfzf

--- cdfzf.sh
+++ cdfzf.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-function cdfzf() {
-    proj_root=$(git rev-parse --show-toplevel 2> /dev/null)
-    if [ -z "$proj_root" ]; then
-        proj_root=$(pwd);
-    fi;
-    cd "$proj_root" || return
+function __cdfzf_impl() {
+    root=$1
+    cd "$root" || return
     dir=$(fd --type=d | fzf --preview "tree {} -L 1 -C" --bind "ctrl-j:down,ctrl-k:up,ctrl-h:preview-up,ctrl-l:preview-down,ctrl-q:abort")
     if [ -z "$dir" ]; then
         cd - || return
         return
     fi;
     cd "$dir" || exit
+}
+
+function cdfzf() {
+    git_root=$(git rev-parse --show-toplevel 2> /dev/null)
+    if [ -z "$git_root" ]; then
+        git_root=$(pwd);
+    fi;
+    [[ $# -eq 0 ]] && root="$git_root" || root=$1
+    __cdfzf_impl "$root"
 }
