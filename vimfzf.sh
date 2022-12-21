a8fabdc1bbc135488b823ca0643951e78cd84743
Kair <kair8m@gmail.com>
2022-12-21 14:48:56 +0100

    add input param to vimfzf

--- vimfzf.sh
+++ vimfzf.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-function vimfzf() {
-    proj_root=$(git rev-parse --show-toplevel 2> /dev/null)
-    if [ -z "$proj_root" ]; then
-        proj_root=$(pwd);
-    fi;
-    cd "$proj_root" || return
+function __vimfzf_impl() {
+    root=$1
+    cd "$root" || return
     file=$(fzf --preview "bat --color=always --style=numbers {-1}" --bind "ctrl-j:down,ctrl-k:up,ctrl-h:preview-up,ctrl-l:preview-down,ctrl-q:abort")
     if [ -z "$file" ]; then
         cd - || return
@@ -13,4 +10,13 @@ function vimfzf() {
     fi;
     nvim "$file"
     cd - || return
+}
+
+function vimfzf() {
+    git_root=$(git rev-parse --show-toplevel 2> /dev/null)
+    if [ -z "$git_root" ]; then
+        git_root=$(pwd);
+    fi;
+    [[ $# -eq 0 ]] && root="$git_root" || root=$1
+    __vimfzf_impl "$root"
 }
