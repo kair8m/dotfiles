6d7822bdba4553e128f2bfef5003341ae84b3015
Kair <kair8m@gmail.com>
2023-02-13 14:16:12 +0100

    add lsd aliases

--- aliases.sh
+++ aliases.sh
@@ -15,3 +15,10 @@ alias gs="git status"
 alias gc="git checkout"
 alias gs="git status"
 alias gl="git log"
+if hash lsd 2>/dev/null; then
+    alias ls="lsd --group-directories-first"
+    alias ll="lsd -l --group-directories-first"
+    alias la="lsd -lA --group-directories-first"
+    alias l="lsd -la --group-directories-first"
+    alias lt="lsd -la --group-directories-first --tree"
+fi
