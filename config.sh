cb251e79e0c739b759ee92690adaac5bb886bae3
Kair <kair8m@gmail.com>
2023-07-16 02:15:02 +0200

    update fzf config

--- config.sh
+++ config.sh
@@ -5,6 +5,12 @@ export FZF_DEFAULT_COMMAND="fd -H --type f"
 export FZF_CTRL_T_COMMAND="fd -H --type f"
 export FZF_ALT_C_COMMAND="fd -H --type f"
 export FZF_DEFAULT_OPTS=" \
+--layout=reverse \
+--height=90% \
+--margin=1 \
+--padding=1 \
+--border \
+--info=inline \
 --color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
 --color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
 --color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284"
