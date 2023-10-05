7de02c911cd0cf35950db2271c3591bc5ceffc0c
Kairzhan <kmuldashev@fyusion.com>
2023-10-05 19:29:24 +0200

    add keybindings to default options

--- config.sh
+++ config.sh
@@ -10,7 +10,8 @@ export FZF_DEFAULT_OPTS=" \
 --info=inline \
 --color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
 --color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
---color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284"
+--color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284 \
+--bind 'ctrl-j:down,ctrl-d:page-down,ctrl-u:page-up,ctrl-k:up,ctrl-h:preview-up,ctrl-l:preview-down,ctrl-q:abort'"
 
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export PYTHONSTARTUP=${HOME}/.pythonrc
